17a3d24e9c2810d1a47f1d0178d993d696a5b27a
Amlan Deep Nandi <99658242+Amlandeep001@users.noreply.github.com>
2024-05-19 13:48:17 +0530

    Added Customer Scalar
    Added Customer Alias Scalar for ID as Key.

--- src/main/java/com/learn/graphqlscalars/config/GraphQlConfig.java
+++ src/main/java/com/learn/graphqlscalars/config/GraphQlConfig.java
@@ -4,15 +4,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.graphql.execution.RuntimeWiringConfigurer;
 
+import graphql.Scalars;
 import graphql.scalars.ExtendedScalars;
+import graphql.schema.GraphQLScalarType;
 
 @Configuration
 public class GraphQlConfig
 {
+	GraphQLScalarType keyAliasScalar = ExtendedScalars
+			.newAliasedScalar("Key")
+			.description("Key for ID Scalar")
+			.aliasedScalar(Scalars.GraphQLID)
+			.build();
+
 	@Bean
 	RuntimeWiringConfigurer runtimeWiringConfigurer()
 	{
 		return wiringBuilder -> wiringBuilder
-				.scalar(ExtendedScalars.GraphQLBigDecimal);
+				.scalar(ExtendedScalars.GraphQLBigDecimal)
+				.scalar(keyAliasScalar);
 	}
 }
